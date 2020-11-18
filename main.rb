1cfb2a6e1f4ef3a1642f41a302ae7e8faa150fc8
jeff-hykin <jeff.hykin@gmail.com>
2020-11-18 13:17:19 -0600

    add comments

--- main.rb
+++ main.rb
@@ -1,34 +1,43 @@
-puts "Whats the path to the .app? (ex:/Applications/Pages.app)"
+puts "Whats the path to the .app? (ex:/Applications/iMovie.app)"
 which_app = gets.chomp # chomp removes the trailing newline
-
+# single quotes cause problems, so replace all the single quotes
+which_app = which_app.gsub(/'/,"'\"'\"'") 
 
 puts "Should I run the commands or show you the commands?\n(choose either \"show\" or \"run\")"
 @show_or_run = gets.chomp # chomp removes the trailing newline
 puts
 
-# basically this whole app just calls this command on a bunch of files/folders
+# create a function that will sign a specific file/folder
+# basically this whole app just calls this function on a bunch of files/folders
 def sign(filepath)
     if @show_or_run =~ /run/
+        # run the command
         puts `sudo codesign -f -s - '#{filepath}'`.chomp
     else
+        # print out the command to run
         puts "sudo codesign -f -s - '#{filepath}'"
     end
 end
 
+# 
 # figure out where the app executable is
+# 
 executable_statment = `codesign -d '#{which_app}' 2>&1`.chomp
 # ^should return something like: Executable=/Applications/Keynote.app/Contents/MacOS/Keynote
+
+# remove the Executable= part
 executable_path = executable_statment.sub(/^Executable=/,"")
 # resign the executable
 puts "This might take a hot minute (if you picked the 'run' option)"
-puts "running main resigning command:"
 sign(executable_path)
 
+# 
 # re-sign (or atleast try to re-sign) all the frameworks
+# 
 path_to_frameworks = "#{which_app}/Contents/MacOS/../Frameworks/"
 all_subfiles_and_folders = `find '#{path_to_frameworks}' -iname "*"`.split("\n")
 list_of_frameworks = `ls -1 '#{path_to_frameworks}'`.split("\n")
-# ^should return something like:
+# ^should end up as something like:
 #     EquationKit.framework
 #     TSAccessibility.framework
 #     TSApplication.framework
@@ -66,12 +75,12 @@ list_of_frameworks = `ls -1 '#{path_to_frameworks}'`.split("\n")
 #     libswiftsimd.dylib
 
 for each_name in list_of_frameworks
-    # its a framework, lets resign it
+    # its a framework, lets resign it's content
     if each_name =~ /\.framework$/
         base_name = each_name.sub(/\.framework$/,"")
         path_to_framework = "#{which_app}/Contents/MacOS/../Frameworks/#{each_name}/Versions/A/#{base_name}"
         sign(path_to_framework)
-    # if it is a dynamic library it also needs resigning 
+    # if it is a dynamic library it also needs re-signing directly
     elsif each_name =~ /\.dylib$/
         path_to_dylib = "#{which_app}/Contents/MacOS/../Frameworks/#{each_name}"
         sign(path_to_dylib)
@@ -84,7 +93,7 @@ puts " Don't leave just yet"
 puts "-------------------------"
 puts "try opening the app. If it still fails, I'll try a more brute-force method"
 puts "Should I try the more brute force method? (yes or no)"
-answer = gets
+answer = gets.chomp
 if answer =~ /y|yes/
     puts "Attempting to sign all the framework files"
     counter = 0
@@ -101,3 +110,7 @@ if answer =~ /y|yes/
         sign(each_name)
     end
 end
+
+puts "--------------------------------------------------"
+puts "        Okay, DONE! Try opening the app!"
+puts "--------------------------------------------------"
